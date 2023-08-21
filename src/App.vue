46ad13b8e85311abb7411200b0cf603d2340e22d
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-21 17:17:36 +0800

    优化使用 `titleTemplate` 设置页面标题的逻辑

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,20 @@
 </template>
 
 <script lang="ts" setup>
+  import { settings } from '@/settings';
+
+  const route = useRoute();
+  const { t } = useI18n();
+
   const theme = useThemeStore();
+
+  // 设置页面标题
+  {
+    const meta = route.meta || {};
+    const titleTemplate = (meta.titleTemplate ?? settings.titleTemplate ?? `:title | ${settings.name}`) as string;
+
+    useHead({
+      title: titleTemplate.replaceAll(':title', t(`${meta.title ?? ''}`)),
+    });
+  }
 </script>
